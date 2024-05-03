276998b1427e9da4a86f229607b30ee20aad0d1a
Launyx <lunyx.castelhano@gmail.com>
2024-05-03 15:02:56 -0300

    first commit

---
+++ src/Program.java
@@ -0,0 +1,9 @@
+public class Program {
+    public static void main(String[] args) throws Exception {
+        
+        String a = "Maria";
+        String b = "Alex";
+
+        System.out.println(a.equals(b));
+    }
+}
