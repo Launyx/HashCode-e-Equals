9fa6b810f29a1904e3e7916a825eb1eb11e6393d
Launyx <lunyx.castelhano@gmail.com>
2024-05-03 15:04:30 -0300

    HashCode apresentando mesmo código

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,11 @@
 public class Program {
     public static void main(String[] args) throws Exception {
-        
-        String a = "Maria";
-        String b = "Alex";
 
-        System.out.println(a.equals(b));
+        String a = "Maria";
+        String b = "Maria";
+
+        //System.out.println(a.equals(b));
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
     }
 }
